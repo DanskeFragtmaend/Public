79d491e11283056ba89054b4cb8e43c224c9ffbd
Paw <paw.baltzersen@fragt.dk>
2022-04-06 10:05:54 +0200

    Removed AuditLogService files

--- src/Df.AuditLogging/Df.AuditLogging/IAuditLogService.cs
+++
@@ -1,9 +0,0 @@
-﻿// namespace Df.AuditLogging;
-//
-// /// <summary>Abstraction between the correlator and the database layer.</summary>
-// public interface IAuditLogService
-// {
-//     Task Insert(Audit audit);
-//     Task Insert(IList<Audit>audits);
-//     Task<IList<Audit>> GetAudits(string dataId, int detailLevel);
-// }
