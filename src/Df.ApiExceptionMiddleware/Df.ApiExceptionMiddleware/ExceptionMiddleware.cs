d14afb2e901f786d4690daa99f979434c97e4a10
Paw <paw.baltzersen@fragt.dk>
2023-01-31 15:44:42 +0100

    Exception middle ware support UnauthorizedAccessException

--- src/Df.ApiExceptionMiddleware/Df.ApiExceptionMiddleware/ExceptionMiddleware.cs
+++ src/Df.ApiExceptionMiddleware/Df.ApiExceptionMiddleware/ExceptionMiddleware.cs
@@ -6,9 +6,9 @@ namespace Df.ApiExceptionMiddleware;
 
 /// <summary>
 /// A middleware for handling exceptions in your API.
-/// The returned StatusCode is always 500 for now.
+/// The returned StatusCode is 401 (unauthorized), 403 (forbidden) or 500 (server error).
 /// The returned ErrorCode is a deterministic hash generated from the stacktrace of the exception.
-/// The ErrorCode logged together with the exception.
+/// The ErrorCode is logged together with the exception.
 /// </summary>
 public class ExceptionMiddleware
 {
@@ -41,12 +41,14 @@ public class ExceptionMiddleware
 
         var message = exception switch
         {
-            AccessViolationException => "Access violation error.",
+            AccessViolationException => "Forbidden.",
+            UnauthorizedAccessException => "Unauthorized access.",
             _ => "Internal Server Error."
         };
 
         context.Response.StatusCode = exception switch
         {
+            UnauthorizedAccessException => (int)HttpStatusCode.Unauthorized,
             AccessViolationException => (int)HttpStatusCode.Forbidden,
             _ => (int)HttpStatusCode.InternalServerError
         };
