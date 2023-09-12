8dcc8128f66cf5aa507ae35281f931f990195e34
Paw <paw.baltzersen@fragt.dk>
2023-09-12 12:20:26 +0200

    Added ServiceControllerExtensions project and class

---
+++ src/Df.ServiceControllerExtensions/Df.ServiceControllerExtensions/ServiceControllerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Df.ServiceControllerExtensions;
+
+public static class ServiceControllerExtensions
+{
+    /// <summary>Extension method to add configuration to the service collection.</summary>
+    /// <returns>Returns the added object.</returns>
+    /// <remarks>This method makes it easier to both add the configuration object to the service collection and get it for other uses in startup.</remarks>
+    public static T AddConfiguration<T>(this IServiceCollection services, IConfiguration configuration) where T : class
+    {
+        var settings = configuration.GetSection(typeof(T).Name).Get<T>();
+        services.AddSingleton(_ => settings);
+        return settings;
+    }
+}
---
+++ src/Df.ServiceControllerExtensions/readme.md
@@ -0,0 +1,4 @@
+# General extension methods for the ServiceCollection class
+
+## Source code
+https://www.nuget.org/packages/Df.ServiceCollectionExtensions/
